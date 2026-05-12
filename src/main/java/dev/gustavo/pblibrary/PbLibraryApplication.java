eca0d2f8457bb76b262facf2ba9bfab68b651885
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2026-05-12 17:13:43 -0300

    chore: setup inicial do projeto

---
+++ src/main/java/dev/gustavo/pblibrary/PbLibraryApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavo.pblibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gustavo/pblibrary/PbLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gustavo.pblibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
