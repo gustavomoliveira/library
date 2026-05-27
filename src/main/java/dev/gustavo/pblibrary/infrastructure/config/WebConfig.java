977c3c77a2a918de3543f747ee202697a9d7226e
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2026-05-27 10:46:03 -0300

    feat(config): add CORS configuration for localhost:5173

---
+++ src/main/java/dev/gustavo/pblibrary/infrastructure/config/WebConfig.java
@@ -0,0 +1,14 @@
+package dev.gustavo.pblibrary.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class WebConfig {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+    }
+}
