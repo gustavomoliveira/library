76a0ab173f719a6376114fb8e57a0102b88854e8
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2026-09-25 18:53:24 -0300

    feat: adiciona manifest k8s do library-frontend, corrige CORS do api-gateway para aceitar multiplas origens

--- api-gateway/src/main/java/dev/gustavo/apigateway/WebConfig.java
+++ api-gateway/src/main/java/dev/gustavo/apigateway/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "http://localhost:30080")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
     }
 }
